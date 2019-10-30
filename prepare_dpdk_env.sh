0539a5393599ea558bb9aafa144fb15c11eba097
Danylo Lubenets <danilarff86@gmail.com>
2019-10-30 13:45:46 +0200

    Adjusted configurations according to the network

--- prepare_dpdk_env.sh
+++ prepare_dpdk_env.sh
@@ -8,4 +8,4 @@ insmod $RTE_SDK/build/kmod/igb_uio.ko
 mount -t hugetlbfs nodev /mnt/huge
 echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
-$RTE_SDK/usertools/dpdk-devbind.py --bind igb_uio 0000:02:00.0
+$RTE_SDK/usertools/dpdk-devbind.py --bind igb_uio 0000:02:00.0 0000:03:00.0
